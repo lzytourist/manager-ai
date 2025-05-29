cb8cdf368122b6d808572411af155140459cea99
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-05-29 16:20:04 +0600

    Signal for updating current balance

--- fintrack/apps.py
+++ fintrack/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FintrackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fintrack'
+
+    def ready(self):
+        import fintrack.signals
---
+++ fintrack/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Transaction
+
+
+@receiver(post_save, sender=Transaction)
+def update_current_balance(instance, **kwargs):
+    """Updates the current balance of the transaction."""
+    last_transaction = (Transaction.objects.filter(user_id=instance.user_id).exclude(pk=instance.pk)
+                        .order_by('-created_at').first())
+
+    instance.balance_after_transaction = instance.amount
+    if last_transaction:
+        if instance.transactino_type == Transaction.Type.BALANCE:
+            instance.balance_after_transaction += last_transaction.balance_after_transaction
+        else:
+            instance.balance_after_transaction -= last_transaction.balance_after_transaction
+    instance.save()
