3f90941c3bcc0929a4792d904651cb661a341da9
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-05-30 18:47:16 +0600

    Transaction pre_save event

--- fintrack/signals.py
+++ fintrack/signals.py
@@ -1,10 +1,10 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from .models import Transaction
 
 
-@receiver(post_save, sender=Transaction)
+@receiver(pre_save, sender=Transaction)
 def update_current_balance(instance, **kwargs):
     """Updates the current balance of the transaction."""
     last_transaction = (Transaction.objects.filter(user_id=instance.user_id).exclude(pk=instance.pk)
@@ -12,8 +12,7 @@ def update_current_balance(instance, **kwargs):
 
     instance.balance_after_transaction = instance.amount
     if last_transaction:
-        if instance.transactino_type == Transaction.Type.BALANCE:
+        if instance.transaction_type == Transaction.Type.BALANCE:
             instance.balance_after_transaction += last_transaction.balance_after_transaction
         else:
-            instance.balance_after_transaction -= last_transaction.balance_after_transaction
-    instance.save()
+            instance.balance_after_transaction = last_transaction.balance_after_transaction - instance.amount
