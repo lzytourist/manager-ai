7a5fab33de714d0fa2b6246d4ff169053fe00246
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-05-30 18:47:43 +0600

    Transaction data table

--- fintrack/urls.py
+++ fintrack/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from fintrack.views import HomePageView
+from fintrack.views import HomePageView, TransactionListJson, TransactionPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('transactions/', TransactionPageView.as_view(), name='transactions'),
+    re_path(r'^datatable/data/$', TransactionListJson.as_view(), name='transactions-list'),
 ]
--- fintrack/views.py
+++ fintrack/views.py
@@ -1,5 +1,40 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.utils.html import escape
 from django.views.generic import TemplateView
+from django_datatables_view.base_datatable_view import BaseDatatableView
+
+from .models import Transaction
 
 
 class HomePageView(TemplateView):
     template_name = 'fintrack/home.html'
+
+
+class TransactionPageView(TemplateView):
+    template_name = 'fintrack/transactions.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TransactionListJson(BaseDatatableView):
+    model = Transaction
+    columns = ['title', 'amount', 'transaction_type', 'created_at']
+    order_columns = ['title', 'amount', '', 'created_at']
+    max_display_length = 100
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_initial_queryset(self):
+        return Transaction.objects.filter(user=self.request.user).order_by('-created_at')
+
+    # def filter_queryset(self, qs):
+    #     search = self.request.GET.get('search[value]', None)
+    #     if search:
+    #         qs = qs.filter(title__icontains=search)
+    #     return qs  # <- Make sure you return this
