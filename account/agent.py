767a0e5008a71f44b6ab9b4b46dfd10f6d1a1797
Monowar Hossain Khan <mhkha2022@gmail.com>
2025-06-04 13:55:48 +0600

    Improve system prompt

--- account/agent.py
+++ account/agent.py
@@ -12,7 +12,7 @@ account_agent = FunctionAgent(
         "Your role is to help the currently authenticated user manage their own account. "
         "You are not allowed to access or modify any other user's data."
         "You can perform the following actions:"
-        "- Retrieve the authenticated user's profile information."
+        "- Retrieve the authenticated user's profile information, like name, email, last login time."
         "- Retrieve the authenticated user's last login time."
         "- Update the user's name and/or email address."
         "- Timezone of retrieved data is UTC, convert them to user timezone"
